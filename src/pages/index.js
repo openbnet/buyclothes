fa5fb94d6138c4eab5267176ec7c8415afaf3ef0
jan <jc@activ.ltd>
2018-10-26 18:01:52 +0800

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+
 import Helmet from "react-helmet";
 import Swiper from 'react-id-swiper';
 
@@ -17,8 +19,10 @@ import clothe3 from '../assets/images/4.3.jpg'
 import clothe4 from '../assets/images/4.4.jpg'
 
 class Homepage extends React.Component {
+    
+    
     render() {
-        const siteTitle = "Figma";
+        const siteTitle = "Prelove.sg";
         
         const params = {
             slidesPerView: 3,
@@ -28,7 +32,7 @@ class Homepage extends React.Component {
               el: '.swiper-pagination',
               clickable: true,
             }
-          };
+        };
         return (
             <Layout>
                 <Helmet title={siteTitle} />
@@ -41,7 +45,7 @@ class Homepage extends React.Component {
                                 </p>
                                 <div className="content">
                                     <p>Trade in your used designer clothing and get instant cash</p>
-                                    <a href="#" onClick={() => console.log("Clicked")} className="contact-us-button button icon">I'm Interested!</a>
+                                    <a href="#contact-us" className="contact-us-button button icon">I'm Interested!</a>
                                 </div>
                             </div>
                             <div className="col-6 flower-dress">
@@ -129,9 +133,8 @@ class Homepage extends React.Component {
                             </div>
                         </div>
                     </div>
-                    
-                <div>
-                    </div>
+                    <div>
+                </div>
                 <div className="container box-3">
                     <h2>Here’s our pricing guide*:</h2>
                     <div className="grid-wrapper  pricing">
@@ -258,7 +261,7 @@ class Homepage extends React.Component {
                         </Swiper>
                     </div>
                     
-                    <div className="hr">
+                    <div className="hr"   id="contact-us">
                         <hr/>
                     </div>
                 </div>
@@ -272,19 +275,6 @@ class Homepage extends React.Component {
                         <div className="row grid-wrapper">
                             <div className="col-6 contact-details">
                             <MailChimpForm/>
-                                {/* <form>
-                                    <label>
-                                        <input type="text" name="name" placeholder="Name"/>
-                                    </label>
-                                    
-                                    <label>
-                                        <input type="text" name="email" placeholder="Email"/>
-                                    </label>
-                                    <input type="text" name="number" placeholder="Contact Number"/>
-                                    <div className="submit-button">
-                                        <a href="#" onClick={() => console.log("button clicked")} className="contact-us-button button">I'm Interested!</a>
-                                    </div>
-                                </form> */}
                             </div>
                             <div className="col-6 address">
                                 <div className="row">
@@ -317,99 +307,11 @@ class Homepage extends React.Component {
                             Ready to start the year fresh?
                         </div>
                         <div>
-                            <a href="#" onClick={() => console.log("button clicked")} className="yes-button button">I'm Interested!</a>
+                            <a href="#contact-us" onClick={() => console.log("button clicked")} className="yes-button button">I'm Interested!</a>
                         </div>
                     </div>
                 </div>
             </div>
-                {/*<section id="one" className="main style1">
-                    <div className="grid-wrapper">
-                        <div className="col-6">
-                            <header className="major">
-                                <h2>Lorem ipsum dolor adipiscing<br />
-                                amet dolor consequat</h2>
-                            </header>
-                            <p>Adipiscing a commodo ante nunc accumsan et interdum mi ante adipiscing. A nunc lobortis non nisl amet vis sed volutpat aclacus nascetur ac non. Lorem curae et ante amet sapien sed tempus adipiscing id accumsan.</p>
-                        </div>
-                        <div className="col-6">
-                            <span className="image fit"><img src={pic01} alt="" /></span>
-                        </div>
-                    </div>
-                </section>
-            
-                 <section id="two" className="main style2">
-                    <div className="grid-wrapper">
-                        <div className="col-6">
-                            <ul className="major-icons">
-                                <li><span className="icon style1 major fa-code"></span></li>
-                                <li><span className="icon style2 major fa-bolt"></span></li>
-                                <li><span className="icon style3 major fa-camera-retro"></span></li>
-                                <li><span className="icon style4 major fa-cog"></span></li>
-                                <li><span className="icon style5 major fa-desktop"></span></li>
-                                <li><span className="icon style6 major fa-calendar"></span></li>
-                            </ul>
-                        </div>
-                        <div className="col-6">
-                            <header className="major">
-                                <h2>Lorem ipsum dolor adipiscing<br />
-                                amet dolor consequat</h2>
-                            </header>
-                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
-                            <p>Blandit faucibus proin. Ac aliquam integer adipiscing enim non praesent vis commodo nunc phasellus cubilia ac risus accumsan. Accumsan blandit. Lobortis phasellus non lobortis dit varius mi varius accumsan lobortis. Blandit ante aliquam lacinia lorem lobortis semper morbi col faucibus vitae integer placerat accumsan orci eu mi odio tempus adipiscing adipiscing adipiscing curae consequat feugiat etiam dolore.</p>
-                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
-                        </div>
-                    </div>
-                </section>
-
-                <section id="three" className="main style1 special">
-                    <div className="grid-wrapper">
-                        <div className="col-12">
-                            <header className="major">
-                                <h2>Adipiscing amet consequat</h2>
-                            </header>
-                            <p>Ante nunc accumsan et aclacus nascetur ac ante amet sapien sed.</p>
-                        </div>
-
-                        <div className="col-4">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
-                            <ul className="actions">
-                                <li><a href="#" className="button">More</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-4">
-                            <span className="image fit"><img src={pic03} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
-                            <ul className="actions">
-                                <li><a href="#" className="button">More</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-4">
-                            <span className="image fit"><img src={pic04} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
-                            <ul className="actions">
-                                <li><a href="#" className="button">More</a></li>
-                            </ul>
-                        </div>
-
-                    </div>
-                </section>
-
-                <section id="four" className="main style2 special">
-                    <div className="container">
-                        <header className="major">
-                            <h2>Ipsum feugiat consequat?</h2>
-                        </header>
-                        <p>Sed lacus nascetur ac ante amet sapien.</p>
-                        <ul className="actions uniform">
-                            <li><a href="#" className="button special">Sign Up</a></li>
-                            <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
-                    </div>
-                </section> */}
             </Layout>
         );
     }
